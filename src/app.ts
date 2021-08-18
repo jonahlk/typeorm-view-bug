564d9b7d7e859d6afaf7263b0829133e2e4cdd3e
jonah <jonah.lakemeier@kreatize.com>
2021-08-18 13:35:31 +0200

    Inital commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express, { Express } from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import { ValidateError } from 'tsoa';
-import { RegisterRoutes } from '../tsoa/routes';
+import 'reflect-metadata';
+import express, {Express} from 'express';
+import helmet             from 'helmet';
+import swaggerUi          from 'swagger-ui-express';
+import {ValidateError}    from 'tsoa';
+import {RegisterRoutes}   from '../tsoa/routes';
 import '@database';
+import {terminal}         from 'terminal-kit';
+
 
 const app: Express = express();
 
@@ -17,14 +18,10 @@ app.set('json spaces', 4);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Handle logs in console during development
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-  app.use(cors());
-}
+
 
 // Handle security and origin in production
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(helmet());
 }
 
@@ -43,25 +40,27 @@ app.use("/docs", swaggerUi.serve, async (req: express.Request, res: express.Resp
  ***********************************************************************************/
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+
   if (err instanceof ValidateError) {
     console.error(`Caught Validation Error for ${req.path}:`, err.fields);
     return res.status(422).json({
-      message: "Validation Failed",
+      message: 'Validation Failed',
       details: err?.fields,
     });
   }
-  if (err instanceof Error) {
-    return res.status(500).json({
-      errorName: err.name,
-      message: err.message,
-      stack: err.stack || 'no stack defined'
-    });
-  }
-  next();
+  return res.status(500).json({
+    errorName: err.name,
+    message: err.message,
+    stack: err.stack || 'no stack defined',
+  });
 });
 
 app.use(function notFoundHandler(_req, res: express.Response) {
-  return res.status(404).send({ message: "Not Found" });
+  return res.status(404).send({message: 'Not Found'});
+});
+
+app.listen(process.env.PORT, () => {
+  terminal.green(`[Express] - Application listening on port: ${process.env.PORT} \n`);
 });
 
 export default app;
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,10 @@
-import { Connection, createConnection, SimpleConsoleLogger } from "typeorm";
-import dotenv from 'dotenv';
+import {Connection, createConnection} from 'typeorm';
+import dotenv                         from 'dotenv';
+import {terminal}                     from 'terminal-kit';
+
 
 dotenv.config({});
+
 class Database {
 
   public connection: Connection;
@@ -12,29 +15,22 @@ class Database {
 
   private connectToDB(): void {
     createConnection({
-      type: envString("mysql", "sqlite"),
-      host: envString(process.env.DATABASE_HOST!, ""),
-      port: envString(Number(process.env.DATABASE_PORT!), 0),
-      username: envString(process.env.DATABASE_USERNAME!, ""),
-      password: envString(process.env.DATABASE_PASSWORD!, ""),
-      database: envString(process.env.DATABASE_NAME!, "./db.sqlite"),
-      entities: [
-        __dirname + "/entity/*.ts",
-        __dirname + "/entity/*.js"
-      ],
-      synchronize: true,
-      logging: false
-    }).then(_con => {
-      this.connection = _con;
-      console.log("Connected to db!!");
-    }).catch(console.error)
+      type: 'postgres',
+      host: process.env.DATABASE_HOST!,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [`${__dirname}/entity/*.ts`],
+      synchronize: false,
+      logging: false,
+    })
+      .then(_con => {
+        this.connection = _con;
+        terminal.cyan(`[Database] - Connected to Database: ${process.env.DATABASE_NAME}\n`);
+      })
+      .catch(console.error);
   }
-
-}
-
-
-function envString<T>(prodString: T, devString: T): T {
-  return process.env.NODE_ENV === 'production' ? prodString : devString
 }
 
 export const db = new Database();
